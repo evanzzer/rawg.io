efa81ccc5a80a3c91cafb919d50e4b15b01e9f9e
evanzzer <evans.hebert@ui.ac.id>
2022-07-09 18:47:30 +0700

    Migrate API KEY to plist file

--- Rawg.io/Core/Utils/Network/APICall.swift
+++ Rawg.io/Core/Utils/Network/APICall.swift
@@ -22,17 +22,31 @@ enum Endpoints {
         case details(id: Int)
         
         public var url: String {
+            let PAGE_SIZE = "25"
+            let SEARCH_SIZE = "25"
+            
+            var apiKey: String {
+                guard let filePath = Bundle.main.path(forResource: "Rawg.io", ofType: "plist") else {
+                    fatalError("Couldn't find file 'Rawg.io.plist'.")
+                }
+                let plist = NSDictionary(contentsOfFile: filePath)
+                guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                    fatalError("Couldn't find key 'API_KEY' in 'Rawg.io.plist'.")
+                }
+                return value
+            }
+            
             switch self {
-            case .popular: return "\(API.baseUrl)?key=\(API_KEY)&page_size=\(PAGE_SIZE)"
+            case .popular: return "\(API.baseUrl)?key=\(apiKey)&page_size=\(PAGE_SIZE)"
             case .search(let query):
                 var components = URLComponents(string: API.baseUrl)!
                 components.queryItems = [
-                    URLQueryItem(name: "key", value: API_KEY),
+                    URLQueryItem(name: "key", value: apiKey),
                     URLQueryItem(name: "page_size", value: SEARCH_SIZE),
                     URLQueryItem(name: "search", value: query)
                 ]
                 return components.string!
-            case .details(let id): return "\(API.baseUrl)/\(id)?key=\(API_KEY)"
+            case .details(let id): return "\(API.baseUrl)/\(id)?key=\(apiKey)"
             }
         }
     }
